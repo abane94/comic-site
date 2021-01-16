3d7768dfdf06ed4bf480c3385850f88775b68123
Aris Husanu <abane94@gmail.com>
2021-01-15 20:29:36 -0500

    add some features to swiper: pagination, navigation

--- client/src/app/book/book-view/book-view.component.ts
+++ client/src/app/book/book-view/book-view.component.ts
@@ -5,6 +5,11 @@ import { ContentService } from '../../shared/services/content.service';
 
 import { Book } from '../../../models';
 
+// import Swiper core and required components
+import SwiperCore, { Navigation, Pagination, Keyboard, HashNavigation, A11y } from 'swiper';
+SwiperCore.use([Navigation, Pagination, Keyboard, HashNavigation]);
+
+
 @Component({
   selector: 'app-book-view',
   templateUrl: './book-view.component.html',
@@ -29,3 +34,15 @@ export class BookViewComponent implements OnInit {
   }
 
 }
+
+
+/** https://github.com/nolimits4web/swiper/blob/master/src/types/swiper-options.d.ts
+To Implement
+  updateOnWindowResize?: boolean;
+  initialSlide?: number;
+  direction?: 'horizontal' | 'vertical';
+ preloadImages?: boolean;
+ lazy?: LazyOptions
+ navigation?: NavigationOptions | boolean;
+hashNavigation
+*/
